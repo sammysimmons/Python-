5975ea95d8f99638f6480a31d7d7c83cc3bcf87d
Samantha Simmons <sammysimmons274@gmail.com>
2020-05-25 22:09:28 +0100

    presenting of messages

--- run.py
+++ run.py
@@ -2,19 +2,33 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    """add messages to messages list"""
+    messages.append("{}: {}".format(username, message)) 
+
+def get_all_messages():
+    """get all the messages and separate them with a 'br'"""
+    return "<br>".join(messages)
+
 @app.route('/')
 def index():
     """Main Page with instructions"""
     return "To send a message use /USERNAME/MESSAGE"
 
- @app.route('/<USERNAME>')
+
+@app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    """ display chat messages"""
+    return "Welcome {0} - {1}".format(username, get_all_messages())
 
 
-@app.route('/<USERNAME>/<MESSAGE>')
+@app.route("/<username>/<message>")
 def send_message(username, message):
-     return "{0}: {1}".format(username, message)
+    """Create a new message and redirect to chat page"""
+    add_messages(username, message)
+    return redirect(username)
 
 app.run(host=os.environ.get('IP', '127.0.0.1'),              
     port=int(os.environ.get('PORT', 5000)),
